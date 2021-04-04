f733f30d6d5fa1a949a2c051946896997198154c
Teu <Teu@MTEU>
2021-04-03 23:03:59 -0400

    Added mdf database and created tables tested working - printed all vehicles

--- TeuCarRepairMgtSys/Program.cs
+++ TeuCarRepairMgtSys/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Data.SqlClient;
+using System.IO;
 
 namespace TeuCarRepairMgtSys
 {
@@ -6,7 +9,40 @@ namespace TeuCarRepairMgtSys
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PrintVehicles();
+        }
+
+        static string GetConnectionString(string connectionStringName)
+        {
+            ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.SetBasePath(Directory.GetCurrentDirectory());
+            configurationBuilder.AddJsonFile("config.json");
+            IConfiguration config = configurationBuilder.Build();
+            return config["ConnectionStrings:"+ connectionStringName];
+        }
+
+
+        static void PrintVehicles()
+        {
+            string cs = GetConnectionString("DatabaseMdf");
+            Console.WriteLine(cs);
+            SqlConnection conn = new SqlConnection(cs);
+            string query = "Select vID,vmake,vmodel,vyear,vcondition from Vehicle";
+
+            SqlCommand cmd = new SqlCommand(query, conn);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                int id = reader.GetInt32(0);
+                string firstName = reader.GetString(1);
+                string lastName = reader.GetString(2);
+                Console.WriteLine($"{id,5} {firstName,-15} {lastName,-15}");
+            }
+            conn.Close();
+
         }
     }
+
 }
+
---
+++ createTables.sql
@@ -0,0 +1,28 @@
+﻿CREATE TABLE Vehicle(
+vID int IDENTITY(1,1) NOT NULL,
+vmake varchar(30) NOT NULL,
+vmodel varchar(30) NOT NULL,
+vyear int NOT NULL,
+vcondition varchar(10) NOT NULL,
+CONSTRAINT "PK_vehicle" PRIMARY KEY  CLUSTERED ("vID")
+);
+
+
+CREATE TABLE Inventory(
+iID int IDENTITY(1,1) NOT NULL,
+vID int NOT NULL,
+vmodel varchar(30) NOT NULL,
+vyear int NOT NULL,
+vcondition varchar(5) NOT NULL,
+CONSTRAINT "PK_inventory" PRIMARY KEY  CLUSTERED ("iID"),
+CONSTRAINT "FK_Vehicle_Vehicle" FOREIGN KEY ("vID") REFERENCES "dbo"."Vehicle" ("vID")
+);
+
+
+CREATE TABLE Repair(
+rID int IDENTITY(1,1) NOT NULL,
+iID int NOT NULL,
+whatToRepair varchar(30) NOT NULL,
+CONSTRAINT "PK_repair" PRIMARY KEY  CLUSTERED ("rID"),
+CONSTRAINT "FK_Inventory_Inventory" FOREIGN KEY ("iID") REFERENCES "dbo"."Inventory" ("iID")
+);
